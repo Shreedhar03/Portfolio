9b2bc093adaf2373851da7e291d117f5aaa1d5e9
Shreedhar Deodatta Urawane <urawane03@gmail.com>
2024-05-25 18:35:38 +0530

    cleanup

--- public/script.js
+++ public/script.js
@@ -51,7 +51,9 @@ function mode_toggle() {
 
         document.querySelectorAll('#nav-link')[i].style.color === 'white' ? document.querySelectorAll('#nav-link')[i].style.color = 'black' : document.querySelectorAll('#nav-link')[i].style.color = 'white'
     }
-
+    document.querySelectorAll('.social-svg').forEach(i => {
+        i.classList.toggle('social-svg-white')
+    })
     document.querySelector('#dark_btn').innerHTML === 'dark_mode' ? document.querySelector('#dark_btn').innerHTML = 'light_mode' : document.querySelector('#dark_btn').innerHTML = 'dark_mode'
 }
 
@@ -82,7 +84,7 @@ const handleLeftMenu = () => {
 window.addEventListener('scroll', () => {
     let height = window.scrollY
     let logo = document.querySelector(".logo")
-    console.log(height)
+    // console.log(height)
     if (height < 190) {
         if (!logo.classList.contains('hidden'))
             logo.classList.add('hidden')
@@ -98,36 +100,57 @@ window.addEventListener('scroll', () => {
 const projects = [
     {
         name: 'UrbanCart',
-        imageSrc: 'urbancart.png',
+        imageSrc: 'images/urbancart.png',
         description: 'An E-commerce website with Razorpay payment gateway',
+        techStack: "React | NodeJs | MongoDB",
         githubLink: 'https://github.com/Shreedhar03/UrbanCart',
         liveLink: 'https://urbancart.cyclic.app'
     },
     {
-        name: 'taskDone',
-        imageSrc: 'taskdone.png',
-        description: 'Full stack todo application with graphical representation',
-        githubLink: 'https://github.com/Shreedhar03/taskDone',
-        liveLink: 'https://taskdone03.web.app'
-    },
-    {
         name: 'ChitChat',
-        imageSrc: 'chitchat.png',
+        imageSrc: 'images/chitchat.png',
         description: 'Realtime chat application using Socket.io',
+        techStack: "NextJs | Tailwind | Socket.io",
         githubLink: 'https://github.com/Shreedhar03/ChitChat',
         liveLink: 'https://chitchat-peach.vercel.app'
     },
     {
+        name: 'SlideSync',
+        imageSrc: 'images/slidesync.png',
+        description: 'Application for controlling slides remotely',
+        techStack: "React | ElectronJs | NodeJs | Socket.io",
+        githubLink: 'https://github.com/slidesync',
+        liveLink: 'https://github.com/slidesync'
+    },
+    {
+        name: 'BillWise',
+        imageSrc: 'images/billwise.png',
+        description: 'Inventory management application for seamless invoices tracking',
+        techStack: "NextJs | Shadcn | PostgreSQL",
+        githubLink: 'https://github.com/Shreedhar03/billwise',
+        liveLink: 'https://billwise.vercel.app/'
+    },
+    {
         name: 'NoteAttendance',
-        imageSrc: 'noteAttendance.png',
+        imageSrc: 'images/noteAttendance.png',
         description: 'Digital attendance marking application',
+        techStack: "React | Firebase | NodeJs",
         githubLink: 'https://github.com/Shreedhar03/NoteAttendance',
         liveLink: 'https://noteattendance13.web.app'
     },
     {
+        name: 'taskDone',
+        imageSrc: 'images/taskdone.png',
+        description: 'Full stack todo application with graphical representation',
+        techStack: "React | MongoDB | Material-Tailwind",
+        githubLink: 'https://github.com/Shreedhar03/taskDone',
+        liveLink: 'https://taskdone03.web.app'
+    },
+    {
         name: 'AccuWeather',
-        imageSrc: 'accuweather.png',
+        imageSrc: 'images/accuweather.png',
         description: 'Application for checking real-time weather conditions & AQI',
+        techStack: "HTML | CSS | JS",
         githubLink: 'https://github.com/Shreedhar03/AccuWeather',
         liveLink: 'https://accuweather13.netlify.app/'
     }
@@ -138,17 +161,18 @@ let projectContianer = document.querySelector('#projects')
 projects.forEach(project => {
     const projectHTML = `
     <div class="project-inner-box cc text-bg-dark">
-    <div class="web-images d-flex justify-content-flex-start align-items-center flex-wrap">
-      <img src=${project.imageSrc} class="projectImage" alt="projectImage">
+        <div class="web-images d-flex justify-content-flex-start align-items-center flex-wrap">
+            <img src=${project.imageSrc} class="projectImage" alt="projectImage">
+        </div>
+        <h4>${project.name}</h4>
+        <h5>${project.description}</h5>
+        <h6>${project.techStack}</h6>
+        <div class="project-btns d-flex justify-content-center align-items-center">
+          <a href=${project.githubLink} target="_blank"><img src="icons/github.svg" alt="github" style="width: 35px;"></a>
+          <a href=${project.liveLink} target="_blank" class="linkIcon"><img src="icons/link.svg" alt="link" style="width: 25px;"></a>
+        </div>
     </div>
-    <h4>${project.name}</h4>
-    <h5>${project.description}</h5>
-    <div class="project-btns d-flex justify-content-center align-items-center">
-      <a href=${project.githubLink} target="_blank"><img src="github.svg" alt="github" style="width: 35px;"></a>
-      <a href=${project.liveLink} target="_blank" class="linkIcon"><img src="link.svg" alt="github" style="width: 25px;"></a>
-    </div>
-  </div>
     `
 
-    projectContianer.innerHTML+= projectHTML
+    projectContianer.innerHTML += projectHTML
 })
